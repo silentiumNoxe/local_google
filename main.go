166709b18e6f07f7ae468416afabfdc3e7f5d29d
Dmytro Ukhin (silentium.noxe) <silentium.noxe@gmail.com>
2025-10-01 12:22:57 +0300

    remove unused code

--- main.go
+++ main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
-	"local_google/html"
 	"local_google/robot"
 	"log/slog"
-	"strings"
 	"sync"
 )
 
@@ -35,47 +33,4 @@ func main() {
 	}
 
 	wg.Wait()
-}
-
-func AnalyzePage(root *html.Node) (*AnalyzeResult, error) {
-	var result = AnalyzeResult{
-		Index: make(map[string]int),
-	}
-
-	var extractContent = func(s string) {
-		s = strings.ToLower(s)
-		entries := strings.Split(s, " ")
-		for _, x := range entries {
-			x = strings.TrimSpace(x)
-			if x == "" {
-				continue
-			}
-			result.Index[x]++
-		}
-	}
-
-	var walker = html.NewWalker(root)
-
-	for {
-		n := walker.Next()
-		if n == nil {
-			break
-		}
-
-		if n.Tag == "a" {
-			uri := n.Attr["href"]
-			if uri != "" {
-				result.Links = append(result.Links, uri)
-			}
-		}
-
-		extractContent(n.Content)
-	}
-
-	return &result, nil
-}
-
-type AnalyzeResult struct {
-	Index map[string]int
-	Links []string
 }
